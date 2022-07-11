870b290f9dbc504e4c114548e72ceb34fa9199fa
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-07-11 18:11:36 +0200

    add nav and banner

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <img src="./banner.png" className="banner-img" />
+      <div className="banner-text">
+        <h1>Online Experiences</h1>
+        <p>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
+      </div>
+    </div>
+  )
+}
