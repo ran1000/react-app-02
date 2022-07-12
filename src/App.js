344cae0f21067ff03ece3737cc6893be8a4a9ba7
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-07-12 11:44:23 +0200

    add props to card components

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
 import React from "react"
-import logo from './logo.svg';
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
 import './App.css';
 
-const experiences = [
-  {
-    image: "strnig",
-    rating: 2,
-    title: "dss",
-    price: 3
-  }
-]
+const experienceA = {
+  image: "https://images.unsplash.com/photo-1530549387789-4c1017266635?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3dpbW1pbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+  rating: 4,
+  title: "Life lessons with John Doe",
+  price: 135,
+}
+
+const experienceB = {
+  image: "https://images.unsplash.com/photo-1631125915902-d8abe9225ff2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2VyYW1pY3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+  rating: 5,
+  title: "Ceramic with Lisa Anderson",
+  price: 50,
+}
+
+const experiences = [];
+experiences.push(experienceA);
+experiences.push(experienceB);
 
 function App() {
+  const cards = experiences.map(item => {
+    return (
+      <Card
+        title={item.title}
+        image={item.image}
+        rating={item.rating}
+        price={item.price}
+      />
+    )
+  })
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <div>
+    < div >
       <Navbar />
       <Hero />
       <div className="card-grid">
-        <Card />
+        {cards}
       </div>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,24 @@
 import React from "react";
 
-export default function Card() {
+export default function Card(props) {
+  // Card Logic
   return (
+    // Card Displayed Information
     <div className="card">
       <div className="soldout-label">
         <h5>SOLD OUT</h5>
       </div>
-      <img src="https://images.unsplash.com/photo-1530549387789-4c1017266635?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3dpbW1pbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60" />
+      <img src={props.image} alt="Experience." />
       <div className="card-info">
         <p>
           <i className="fa-solid fa-star"></i>
-          5.0(6) - USA</p>
+          {props.rating}.0(6) - USA</p>
         <p>
-          Life lessons with Katie Zaferes
+          {props.title}
         </p>
         <p>
           <strong>
-            From $135
+            From ${props.price}
           </strong>
           / person
         </p>
